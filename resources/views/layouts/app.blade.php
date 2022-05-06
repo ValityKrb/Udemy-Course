817af5ae24e44d3aa7cc9b24dad53efce0e554c7
ValityKrb <walterjustin2110@gmail.com>
2022-05-06 21:32:21 +0200

    nav links updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Startseite</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/info">Information</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
