64232e403339443d32c9a482504e2b94c10ab4f3
ValityKrb <walterjustin2110@gmail.com>
2022-05-11 23:01:41 +0200

    migration für udemys Tabelle

--- database/migrations/2022_05_11_204713_create_udemies_table.php
+++ database/migrations/2022_05_11_204713_create_udemies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('udemies', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->text("beschreibung");
             $table->timestamps();
         });
     }
