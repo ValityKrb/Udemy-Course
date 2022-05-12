ce7349aee3308a17557c4858a81691974004e3ee
ValityKrb <walterjustin2110@gmail.com>
2022-05-12 16:01:12 +0200

    typo

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resources(['hobby' => 'App\Http\Controllers\UdemyController']);
+Route::resources(['udemy' => 'App\Http\Controllers\UdemyController']);
